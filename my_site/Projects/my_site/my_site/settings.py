caab21e56ef7049962f0f510e5e018d8ab07a662
harsh soni <harshsoni6011@gmail.com>
2025-03-20 16:38:51 +0530

    added css to base and index page

--- my_site/Projects/my_site/my_site/settings.py
+++ my_site/Projects/my_site/my_site/settings.py
@@ -126,3 +126,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Added by me
+STATICFILES_DIRS = [
+    
+    BASE_DIR / "static"
+    
+]
