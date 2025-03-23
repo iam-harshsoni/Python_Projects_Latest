645d6f7fdccad3d5a6ac7da501965240c1612499
harsh soni <harshsoni6011@gmail.com>
2025-03-23 20:54:05 +0530

    added new feature, using sessions there will be Read Later page, where uses can store the posts to read later

--- my_site/Projects/my_site/blog/urls.py
+++ my_site/Projects/my_site/blog/urls.py
@@ -6,5 +6,6 @@ from . import views
 urlpatterns = [
     path('', views.IndexView.as_view(), name="index"),
     path('posts/', views.PostView.as_view(), name="all-posts"),
-    path('posts/<slug:slug>/', views.PostDetailView.as_view(), name="post-detail")
+    path('posts/<slug:slug>/', views.PostDetailView.as_view(), name="post-detail"),
+    path('read-later', views.ReadLaterView.as_view(), name="read-later")
 ]
