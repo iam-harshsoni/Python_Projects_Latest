2ffde85869bb9f7c6e15a3e980574af120b23b64
harsh soni <harshsoni6011@gmail.com>
2025-03-24 21:11:39 +0530

    chore: configured static-files.config to handle user upload files by webserver rather than django doing it.

--- my_site/Projects/my_site/my_site/settings.py
+++ my_site/Projects/my_site/my_site/settings.py
@@ -15,7 +15,8 @@ from pathlib import Path
 from dotenv import load_dotenv
 from os import getenv
  
-load_dotenv()
+load_dotenv()  # Load environment variables from .env
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
