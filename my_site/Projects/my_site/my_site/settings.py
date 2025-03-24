0cbea6da59783a8d07c041f1f78eabcf9cf85916
harsh soni <harshsoni6011@gmail.com>
2025-03-24 16:38:49 +0530

    chore: configure setting.py to use env variables and added django.config to be used on elastic beanstalk

--- my_site/Projects/my_site/my_site/settings.py
+++ my_site/Projects/my_site/my_site/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from os import getenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,10 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&l0efa8rubrppq1hmvsd*5%@efj^!f)2($=3_r9tl)997m%_^_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.getenv("IS_DEVELOPMENT", True)
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    os.getenv("APP_HOST")
+    ]
 
 # Application definition
 
