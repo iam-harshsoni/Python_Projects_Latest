0bc9fce169efcb4270e313a1e48ef7fc08899677
harsh soni <harshsoni6011@gmail.com>
2025-03-24 13:15:38 +0530

    Configure Django to collect and serve static files - Updated `settings.py` to collect static files into `staticfiles/` - Modified `urls.py` to serve static files correctly

--- my_site/Projects/my_site/my_site/settings.py
+++ my_site/Projects/my_site/my_site/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&l0efa8rubrppq1hmvsd*5%@efj^!f)2($=3_r9tl)997m%_^_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -120,6 +120,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
 
 # Default primary key field type
